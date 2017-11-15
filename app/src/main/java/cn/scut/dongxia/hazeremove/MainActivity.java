5cffc8d5b0d659e9df70b9117c50a3345e6b55a8
Wu Dongxia <361722445@qq.com>
2017-11-15 19:24:46 +0800

    实现Android Camera预览和拍照功能
    - 成功移植Camera2 Demo
    - 实现预览和拍照功能

---
+++ app/src/main/java/cn/scut/dongxia/hazeremove/MainActivity.java
@@ -0,0 +1,33 @@
+package cn.scut.dongxia.hazeremove;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+        if (null == savedInstanceState){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frame_layout,CameraFragment.newInstance())
+                    .commit();
+        }
+    }
+
+
+    private void initView(){
+
+    }
+}
